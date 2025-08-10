ee57b4137c4ce69e5f3e20af899b475e65168b89
sarahjain6 <sarahjain62005@gmail.com>
2025-08-10 19:37:21 +0530

    Add missing files

--- src/app/(defaultLayout)/service/webdev/page.jsx
+++ src/app/(defaultLayout)/service/webdev/page.jsx
@@ -1,4 +1,4 @@
-import Web from '@/app/Components/CaseDetails/Web';
+import Web from '@/app/Components/Details/Web';
 import BreadCumb from '@/app/Components/Common/BreadCumb';
 import ContactInfo4 from '@/app/Components/ContactInfo/ContactInfo4';
 import React from 'react';
